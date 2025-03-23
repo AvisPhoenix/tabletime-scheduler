fc2254478a5389c62b48b022e0b979bac2773eca
Avis Phoenix <avis.phoenix@gmail.com>
2025-03-23 07:25:49 -0600

    add translation data ts

---
+++ tabletime-scheduler_es.ts
@@ -0,0 +1,436 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en_US">
+<context>
+    <name>ActivititesManager</name>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulario</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="32"/>
+        <source>New Activity</source>
+        <translation>Nueva Actividad</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="41"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="51"/>
+        <source>Classification</source>
+        <translation>Clasificación</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="76"/>
+        <source>Add</source>
+        <translation>Agregar</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="105"/>
+        <source>Activities</source>
+        <translation>Actividades</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.ui" line="123"/>
+        <source>Unclassified</source>
+        <translation>Sin clasificar</translation>
+    </message>
+    <message>
+        <location filename="ui/activititesmanager.cpp" line="86"/>
+        <source>Error: Define the activity name.</source>
+        <translation>Error: Debes definir el nombre de la actividad.</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigurationDlg</name>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialogo</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="30"/>
+        <source>Activities</source>
+        <translation>Actividades</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="36"/>
+        <source>Font Color:</source>
+        <translation>Color de letra:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="76"/>
+        <location filename="ui/configurationdlg.ui" line="194"/>
+        <source>Dark</source>
+        <translation>Oscuro</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="116"/>
+        <source>Color Theme:</source>
+        <translation>Color del tema:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="140"/>
+        <source>Opacity:</source>
+        <translation>Opacidad:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="154"/>
+        <location filename="ui/configurationdlg.ui" line="232"/>
+        <source>Ligth</source>
+        <translation>Claro</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="164"/>
+        <source>Overload Color:</source>
+        <translation>Color en translape:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="188"/>
+        <source>Grid</source>
+        <translation>Tabla</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="204"/>
+        <source>Grid Interlines:</source>
+        <translation>Lineas intermedias:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="218"/>
+        <source>Headers:</source>
+        <translation>Encabezados:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="225"/>
+        <source>Grid Lines:</source>
+        <translation>Lineas:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="249"/>
+        <source>Hour Text:</source>
+        <translation>Horas:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="277"/>
+        <source>Header Opacity:</source>
+        <translation>Opacidad de encabezados:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.ui" line="298"/>
+        <source>Header Text:</source>
+        <translation>Texto de encabezados:</translation>
+    </message>
+    <message>
+        <location filename="ui/configurationdlg.cpp" line="95"/>
+        <source>Pick user color</source>
+        <translation>Selecciona el color del usuario</translation>
+    </message>
+</context>
+<context>
+    <name>ImportDlg</name>
+    <message>
+        <location filename="ui/importdlg.ui" line="23"/>
+        <source>Import Schedule File Data</source>
+        <translation>Importar datos de horarios</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="34"/>
+        <source>Select File</source>
+        <translation>Seleccionar archivo</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="43"/>
+        <source>Choose what to import</source>
+        <translation>Elije qué importar</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="49"/>
+        <source>Activities</source>
+        <translation>Actividades</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="56"/>
+        <source>Users</source>
+        <translation>Usuarios</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="66"/>
+        <source>Merge all the content (activities, users, scheduled activities)</source>
+        <translation>Mezclar todo el contenido (actividades, usuarios, horarios)</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="76"/>
+        <source>Priority</source>
+        <translation>Prioridad</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="82"/>
+        <source>Current file</source>
+        <translation>Archivo actual</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="92"/>
+        <source>Imported File</source>
+        <translation>Archivo importado</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="118"/>
+        <source>Start</source>
+        <translation>Iniciar</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.ui" line="141"/>
+        <source>Errors</source>
+        <translation>Errores</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="47"/>
+        <source>Warning</source>
+        <translation>Advertencia</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="48"/>
+        <source>You cannot closed this dialog until the importation finish</source>
+        <translation>No puedes cerrar este dialogo hasta que la importanción termine</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="62"/>
+        <source>No data to import</source>
+        <translation>No hay datos a importar</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="63"/>
+        <source>You need to choose data to import, activities, users or merge.</source>
+        <translation>Necesitas elegir datos a importar: actividades, usuarios, o mezclar todo.</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="70"/>
+        <source>Pick file to import</source>
+        <translation>Elije un archivo para importar</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="72"/>
+        <source>Schedule YAML Files (*.%1)</source>
+        <translation>Archivos YAML de horarios(*.%1)</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="87"/>
+        <source>Warning: The owner color %1 is duplicated.</source>
+        <translation>Advertencia: El color %1 esta duplicado.</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="91"/>
+        <source>Warning: Found a duplicated scheduled activity: { Name: %1, Classification: %2, Owner: %3, time: { start: %4, end: %5, dayOfWeek: %6 }.</source>
+        <translation>Advertencia: Se encontro actividades en el horario duplicadas: {Nombre: %1, Clasificación: %2, Dueño: %3, Hora: {Inicio: %4, Final: %5, díaSemanal: %6}}.</translation>
+    </message>
+    <message>
+        <location filename="ui/importdlg.cpp" line="95"/>
+        <source>Done</source>
+        <translation>Listo</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="24"/>
+        <source>Tabletime Scheduler</source>
+        <translation>Agenda de horarios</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="68"/>
+        <location filename="mainwindow.cpp" line="197"/>
+        <source>Save Schedule</source>
+        <translation>Guardar horario</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>Schedule YAML Files (*.%1)</source>
+        <translation>Archivos YAML de horarios(*.%1)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="76"/>
+        <source>Error while trying to save the file %1:
+%2</source>
+        <translation>Error al intentar guardar el archivo %1:
+%2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="116"/>
+        <source>Open Schedule</source>
+        <translation>Abrir horario</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="127"/>
+        <source>Error while trying to load the file %1:
+%2</source>
+        <translation>Error al intentar cargar el archivo%1:
+%2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="187"/>
+        <source>New Schedule</source>
+        <translation>Nuevo horario</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="192"/>
+        <source>Load Schedule</source>
+        <translation>Cargar horario</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="202"/>
+        <source>Import file data</source>
+        <translation>Importar datos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="207"/>
+        <source>Configuration</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="217"/>
+        <source>Menu</source>
+        <translation>Menú</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Monday</source>
+        <translation>Lunes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Tuesday</source>
+        <translation>Martes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Wednesday</source>
+        <translation>Miércoles</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Thursday</source>
+        <translation>Jueves</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Friday</source>
+        <translation>Viernes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Saturday</source>
+        <translation>Sábado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>Sunday</source>
+        <translation>Domingo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="268"/>
+        <source>Activities</source>
+        <translation>Actividades</translation>
+    </message>
+</context>
+<context>
+    <name>NewUserForm</name>
+    <message>
+        <location filename="ui/newuserform.ui" line="20"/>
+        <source>Create new user</source>
+        <translation>Crear un nuevo usuario</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.ui" line="26"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.ui" line="36"/>
+        <source>Color</source>
+        <translation>Color</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.ui" line="58"/>
+        <source>Pick Color</source>
+        <translation>Elije un color</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.ui" line="67"/>
+        <source>Image</source>
+        <translation>Imagen</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.ui" line="76"/>
+        <source>Select Image</source>
+        <translation>Selecciona una imagen</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.cpp" line="63"/>
+        <source>Pick user color</source>
+        <translation>Elije un color de usuario</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.cpp" line="69"/>
+        <source>Choose a image file</source>
+        <translation>Elije un archivo de imagen</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.cpp" line="71"/>
+        <source>Pictures (%1)</source>
+        <translation>Imágenes (%1)</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.cpp" line="117"/>
+        <source>We need a name for this user.</source>
+        <translation>Se necesita un nombre de usuario.</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.cpp" line="125"/>
+        <source>This user name is already choose, you cannot duplicate the user name.</source>
+        <translation>Ese nombre de usuario ya ha existe, no puedes repetir nombres de usuario.</translation>
+    </message>
+    <message>
+        <location filename="ui/newuserform.cpp" line="126"/>
+        <source>This user color is already choose, you cannot duplicate the user color.</source>
+        <translation>Ese color de usuario ya ha sido elejido, no puedes repetir el color de usuario.</translation>
+    </message>
+</context>
+<context>
+    <name>OwnerSelectionButton</name>
+    <message>
+        <location filename="ui/ownerselectionbutton.cpp" line="12"/>
+        <source>Add new user</source>
+        <translation>Agregar nuevo usuario</translation>
+    </message>
+</context>
+<context>
+    <name>ScheduledActivityMenu</name>
+    <message>
+        <location filename="ui/menu/scheduledactivitymenu.cpp" line="8"/>
+        <source>Start time</source>
+        <translation>Hora de inicio</translation>
+    </message>
+    <message>
+        <location filename="ui/menu/scheduledactivitymenu.cpp" line="13"/>
+        <source>End time</source>
+        <translation>Hora final</translation>
+    </message>
+    <message>
+        <location filename="ui/menu/scheduledactivitymenu.cpp" line="18"/>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+</context>
+<context>
+    <name>ViewSelectionButton</name>
+    <message>
+        <location filename="ui/viewselectionbutton.cpp" line="10"/>
+        <source>View selection</source>
+        <translation>Ver selección</translation>
+    </message>
+</context>
+</TS>
